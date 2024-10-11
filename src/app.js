29c58575bcab5ab442615747ae764268e822f192
quanvuongday <quanthuy21299@gmail.com>
2024-10-11 13:54:27 +0300

    change port finder

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const path = require('path');
+const portfinder = require('portfinder');
 
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Route for the home page
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+    portfinder.getPort((err, port) => {
+        if (err) {
+            console.error('Could not get a free port', err);
+            process.exit(1);
+        }
+
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
+    });
 }
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /', () => {
-  it('responds with Hello, World!', async () => {
+  it('responds with HTML containing Hello, World!', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ message: 'Hello, World!' });
+    expect(response.headers['content-type']).toMatch(/html/);
+    expect(response.text).toContain('Hello, World!');
   });
 });
